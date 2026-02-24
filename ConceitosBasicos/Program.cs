f8bb1c2d9cb8ff18549c67653e3d608d80c2f50b
rosenclever <rosenclever@gmail.com>
2026-02-24 20:52:29 -0300

    tratando entrada de idade inválida

--- ConceitosBasicos/Program.cs
+++ ConceitosBasicos/Program.cs
@@ -1,12 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 int idade;
-Console.WriteLine("Digite a sua idade: ");
-idade = Convert.ToInt32(Console.ReadLine());
-if (idade >= 18)
+bool idadeValida = false;
+while (!idadeValida)
 {
-    Console.WriteLine("Você é maior de idade.");
+    Console.WriteLine("Digite a sua idade: ");
+    try
+    {
+        idade = Convert.ToInt32(Console.ReadLine());
+        idadeValida = true;
+        if (idade >= 18)
+        {
+            Console.WriteLine("Você é maior de idade.");
+        }
+        else
+        {
+            Console.WriteLine("Você é menor de idade.");
+        }
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("O valor informado deve ser em algarismos albébricos. NÚMEROS!");
+    }
 }
-else
-{
-    Console.WriteLine("Você é menor de idade.");
-}
+
+
+
