f4eadefaa07483fcbf42886683c46bac0fcde1fc
rosenclever <rosenclever@gmail.com>
2026-03-03 19:58:18 -0300

    classe cliente com atributos e propriedades (nome e cpf)

---
+++ ConceitosPOO/Cliente.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceitosPOO
+{
+    public class Cliente
+    {
+        private string _nome;
+        private string _cpf;
+        public string Nome 
+        {
+            get
+            {
+                return _nome;
+            }
+            set
+            {
+                if (value.Length != 0)
+                    _nome = value;
+                else
+                    throw new Exception("O nome não pode estar vazio");
+            } 
+        }
+        public string Cpf 
+        {
+            get => _cpf; 
+            set
+            {
+                if(value.Length == 11)
+                    _cpf = value;
+                else
+                {
+                    throw new Exception("O CPF deve ter exatamente 11 dígitos!");
+                }
+            }
+        }
+    }
+}
---
+++ ConceitosPOO/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConceitosPOO;
+
+Console.WriteLine("Hello, World!");
+Cliente cliente;
+cliente = new Cliente();
+cliente.Nome = "";
+Console.WriteLine($"Nome do cliente: {cliente.Nome}");
