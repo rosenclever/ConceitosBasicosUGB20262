95368648d4ca5f61576fbbd7c435c7fc5700b051
rosenclever <rosenclever@gmail.com>
2026-03-10 19:00:37 -0300

    classe agencia

---
+++ ConceitosPOO/Agencia.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceitosPOO
+{
+    public class Agencia
+    {
+        private int _numero;
+        private string _nome;
+        private string _telefone;
+        public string Nome { 
+            get => _nome;
+            set
+            {
+                _nome = value;
+            } 
+        }
+        public string Telefone { 
+            get => _telefone; 
+            set
+            {
+                _telefone = value;
+            }
+                 
+        }
+    }
+}
