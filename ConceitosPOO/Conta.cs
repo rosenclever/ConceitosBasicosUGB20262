96a73b00429ef8b463cfe99bd765105fd1bc114e
rosenclever <rosenclever@gmail.com>
2026-03-24 21:35:32 -0300

    relacionamento agencia

--- ConceitosPOO/Conta.cs
+++ ConceitosPOO/Conta.cs
@@ -13,11 +13,20 @@ namespace ConceitosPOO
          * faça com que uma conta possa ser criada ou 
          * informando o numero da  conta e seu saldo ou
          * informando o numero da conta, seu saldo e seu titular.
-         * tota conta deve obrigatoriamente pertencer a uma agencia.
+         * toda conta deve obrigatoriamente pertencer a uma agencia.
          */
         private long _numero;
         private decimal _saldo;
         private Cliente _titular;
+
+        private Agencia _agencia;
+
+        public Agencia Agencia
+        {
+            get { return _agencia; }
+            set { _agencia = value; }
+        }
+
 
         public Cliente Titular
         {
